09c62d7402458e062e35d62a7ba3bf64bd549135
JeremyColegrove2U <>
2022-06-08 22:41:12 -0500

    update

--- src/components/dares.js
+++ src/components/dares.js
@@ -20,11 +20,12 @@ function Dares(props) {
         </div>
         
     </div>
-    <div className="container wrap">
+    <div className="container wrap padding">
         {questions && questions.map((item, index) => {
             
             return <div onClick={()=>remove(index, type)} key={index} className="item">{item}</div>
         })}
+        {questions && questions.length === 0 && <div>click + to add a {type===0?"dare":"truth"}</div>}
     </div>
 
     </div>
